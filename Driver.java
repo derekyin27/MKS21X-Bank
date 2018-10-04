501be9645b69c36e2134ccb5958660f67b63696a
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-10-03 20:31:39 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,17 @@
-
+public class Driver{
+  public static void main(String[]args){
+    BankAccount a = new BankAccount(6291, 300.00, "herro");
+    System.out.println("Your Balance is: " + a.getBalance());
+    a.newPassword("oof");
+    System.out.println("Your new password is:" + onePassword);
+    System.out.print(a);
+    a.deposit(30.00);
+    System.out.print(a);
+    a.withdraw(100.00);
+    System.out.print(a);
+    a.withdraw(9999999.00);
+    System.out.print(a);
+  }
+}
+    
+      
