02371c9d496a2217d02095614e51a41396a0017f
derekyin27 <43381977+derekyin27@users.noreply.github.com>
2018-10-03 20:15:25 -0400

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,1 +1,24 @@
-
+public class BankAccount{
+  private int oneID;
+  private double oneBalance;
+  private String onePassword;
+  public BankAccount(int accountID, double balance, String password) {
+    oneID = accountID;
+    oneBalance = balance;
+    onePassword = password;
+  }
+  
+  public String toString() {
+    System.out.println(accountID + "  " + balance);
+  }
+  
+  public double getBalance() {
+    return oneBalance;
+  }
+  
+  public int getAccountID() {
+    return oneID;
+  }
+  
+  
+    
